03d21d7b1606cd6f1027a1b64f5f26b5750c8404
Bohdan-Nitro <bogdantrogonsky@gmail.com>
2020-01-30 21:00:24 +0200

    UPD: Gulp setup.

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps"),
+    browserSync = require("browser-sync").create();
+
+
+var paths = {
+    styles: {
+        src: "sass/styles.sass",
+        dest: "./"
+    }
+};
+
+function style() {
+    return (
+        gulp
+            .src(paths.styles.src)
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .on("error", sass.logError)
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(paths.styles.dest))
+            .pipe(browserSync.stream())
+    );
+}
+
+function reload() {
+    browserSync.reload();
+}
+
+function watch() {
+
+    browserSync.init({
+        // You can tell browserSync to use this directory and serve it as a mini-server
+        server: {
+            baseDir: "./"
+        }
+        // If you are already serving your website locally using something like apache
+        // You can use the proxy setting to proxy that instead
+        // proxy: "yourlocal.dev"
+    });
+
+    gulp.watch(paths.styles.src, style);
+    gulp.watch("path/to/html/*.html", reload);
+
+}
+
+exports.style = style;
+exports.watch = watch;
---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+function sayHi() {
+    console.log('Pari Match | Test task for CLICABLE\n Autor: Bohdan Torgonsky');
+}
+
+$(document).ready(function () {
+
+    sayHi();
+
+});
